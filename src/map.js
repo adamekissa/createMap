ba02b90a885ec43eafae3927deb258b1fdb0b038
OluC94 <coker.olu94@gmail.com>
2024-06-05 11:19:01 +0100

    pass all tests

--- src/map.js
+++ src/map.js
@@ -1,4 +1,13 @@
 function map(inputArray, callBackFn){
+    const results = [];
+    for (let element of inputArray){
+        results.push(callBackFn(element));
+    }
+    return results;
 }
 
 export {map};
+
+/*
+
+*/
